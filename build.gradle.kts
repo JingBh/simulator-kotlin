7d1f8e091920acc456a198d28f4d139e924cf0d5
JingBh <jingbohao@yeah.net>
2022-01-22 00:16:39 +0800

    chore: add shadow jar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("jvm") version "1.6.10"
     application
 }
