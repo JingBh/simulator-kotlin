f2cc9f27ad5ab9e6942de194ef77bfda9caa5200
JingBh <jingbohao@yeah.net>
2022-01-22 08:44:58 +0800

    feat: add timer on player

--- src/main/kotlin/io/github/jingbh/simulator/player/Player.kt
+++ src/main/kotlin/io/github/jingbh/simulator/player/Player.kt
@@ -1,7 +1,7 @@
 package io.github.jingbh.simulator.player
 
 import io.github.jingbh.simulator.World
-import org.jetbrains.skia.Canvas
+import org.jetbrains.skia.*
 import org.jetbrains.skiko.GenericSkikoView
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkikoView
@@ -9,6 +9,7 @@ import java.awt.Dimension
 import javax.swing.JFrame
 import javax.swing.WindowConstants
 import kotlin.math.floor
+import kotlin.math.round
 
 class Player(
     private val frames: List<World>,
@@ -23,6 +24,12 @@ class Player(
 
     private val skiaLayer = SkiaLayer()
 
+    private val typeface = Typeface.makeDefault()
+    private val font = Font(typeface)
+    private val paint = Paint().apply {
+        color = Color.BLACK
+    }
+
     private var startTime = -1L
 
     init {
@@ -32,7 +39,14 @@ class Player(
                 val nanoTimePassed = nanoTime - startTime
                 val time = (nanoTimePassed / 1_000_000).toDouble() / 1000.0 / timeScale
                 val cycleTime = frames.size * recordRate
-                val frameN = floor((time % cycleTime) / recordRate).toInt()
+                val timeInCycle = time % cycleTime
+                val frameN = floor(timeInCycle / recordRate).toInt()
+
+                val timeTextLine = TextLine.make("Time: ${round(timeInCycle * 100) / 100.0}s", font)
+                val frameTextLine = TextLine.make("Frame: $frameN", font)
+                canvas.drawTextLine(timeTextLine, 10F, 10F, paint)
+                canvas.drawTextLine(frameTextLine, 10F, 28F, paint)
+
                 frames[frameN].entities.forEach { entity ->
                     entity.draw(canvas)
                 }
